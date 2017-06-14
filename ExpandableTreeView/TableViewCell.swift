4759b2963c826b99eda45d7659e41362f6137eac
Pratyush Pratik <pratyushpratik@OSX42.local>
2017-06-14 12:12:53 +0530

    commit

--- ExpandableTreeView/TableViewCell.swift
+++ ExpandableTreeView/TableViewCell.swift
@@ -16,10 +16,25 @@ class TableViewCell: UITableViewCell {
         // Initialization code
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    var additionButtonActionBlock : ((TableViewCell) -> Void)?;
+    
+    func setup(withTitle title: String, level : Int, additionalButtonHidden: Bool) {
+        lbl.text = title
+        
+        let backgroundColor: UIColor
+        if level == 0 {
+            backgroundColor = UIColor(red: 247.0/255.0, green: 247.0/255.0, blue: 247.0/255.0, alpha: 1.0)
+        } else if level == 1 {
+            backgroundColor = UIColor(red: 209.0/255.0, green: 238.0/255.0, blue: 252.0/255.0, alpha: 1.0)
+        } else {
+            backgroundColor = UIColor(red: 224.0/255.0, green: 248.0/255.0, blue: 216.0/255.0, alpha: 1.0)
+        }
+        
+        self.backgroundColor = backgroundColor
+        self.contentView.backgroundColor = backgroundColor
+        
+        let left = 11.0 + 20.0 * CGFloat(level)
+        self.lbl.frame.origin.x = left
     }
     
 }
--- ExpandableTreeView/ViewController.swift
+++ ExpandableTreeView/ViewController.swift
@@ -17,20 +17,51 @@ enum Level {
     case Level3
 }
 
+struct DataObject {
+    var name: String?
+    var children: [DataObject]?
+    
+    init (name: String?) {
+        self.name = name ?? ""
+    }
+    
+    init(name: String?, children: [DataObject]?) {
+        self.name = name ?? ""
+        self.children = children ?? []
+    }
+}
+
 class ViewController: UIViewController, RATreeViewDataSource, RATreeViewDelegate{
-    private var treeView: RATreeView!
+    
+    private var treeTableView: RATreeView!
     private let xibName = "TableViewCell"
+    var data = [DataObject]()
+    
+    
+    convenience init() {
+        self.init(nibName : nil, bundle: nil)
+    }
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        data = ViewController.commonInit()
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        data = ViewController.commonInit()
+        super.init(coder: aDecoder)
+    }
     
     override dynamic func viewDidLoad() {
-        treeView = RATreeView(frame: self.view.bounds)
-        treeView.delegate = self
-        treeView.dataSource = self
+        treeTableView = RATreeView(frame: self.view.bounds)
+        treeTableView.delegate = self
+        treeTableView.dataSource = self
         
-        treeView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        view.addSubview(treeView)
-        treeView.reloadData()
+        treeTableView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.addSubview(treeTableView)
+        treeTableView.reloadData()
         
-        treeView.register( UINib(nibName: xibName, bundle: nil), forCellReuseIdentifier: xibName)
+        treeTableView.register( UINib(nibName: xibName, bundle: nil), forCellReuseIdentifier: xibName)
     }
     
     func treeView(_ treeView: RATreeView, numberOfChildrenOfItem item: Any?) -> Int {
@@ -41,25 +72,76 @@ class ViewController: UIViewController, RATreeViewDataSource, RATreeViewDelegate
 //        default:
 //            <#code#>
 //        }
-        return 3
+        if let item = item as? DataObject {
+            return item.children?.count ?? 0
+        } else {
+            return self.data.count
+        }
+//        return 2
     }
     
     func treeView(_ treeView: RATreeView, cellForItem item: Any?) -> UITableViewCell {
-        let cell = treeView.dequeueReusableCell(withIdentifier: xibName) as? TableViewCell
         
-        cell?.lbl.text = "hey"
-        return cell!
+        
+//        cell?.lbl.text = "hey"
+        
+        guard let cell = treeView.dequeueReusableCell(withIdentifier: xibName) as? TableViewCell, let item = item as? DataObject else { return UITableViewCell() }
+        
+        let level = treeView.levelForCell(forItem: item)
+        let detailsText = "Number of children \(item.children?.count ?? 0)"
+        cell.selectionStyle = .none
+        cell.setup(withTitle: item.name ?? "", level: level, additionalButtonHidden: false)
+        
+        return cell
     }
     
     func treeView(_ treeView: RATreeView, child index: Int, ofItem item: Any?) -> Any {
-        return index
-    }
-    
-    func treeView(_ treeView: RATreeView, indentationLevelForRowForItem item: Any) -> Int {
-       return 2
+        if let item = item as? DataObject {
+            return item.children?[index] ?? ""
+        } else {
+            return data[index] as AnyObject
+        }
     }
     
     func treeView(_ treeView: RATreeView, heightForRowForItem item: Any) -> CGFloat {
         return 80
     }
+}
+
+
+extension ViewController {
+    
+    static func commonInit() -> [DataObject] {
+        
+        let phone1 = DataObject(name: "Phone 1")
+        let phone2 = DataObject(name: "Phone 2")
+        let phone3 = DataObject(name: "Phone 3")
+        let phone4 = DataObject(name: "Phone 4")
+        let phones = DataObject(name: "Phones", children: [phone1, phone2, phone3, phone4])
+        
+        let notebook1 = DataObject(name: "Notebook 1")
+        let notebook2 = DataObject(name: "Notebook 2")
+        
+        let notebook = DataObject(name: "Notebook", children: [notebook1, notebook2])
+        
+        let computer2 = DataObject(name: "Computer 2")
+        let computer3 = DataObject(name: "Computer 3")
+        let computers = DataObject(name: "Computers", children: [computer2, computer3])
+        let object = DataObject(name: "objects", children: [notebook, computers])
+        
+        
+        let cars = DataObject(name: "Cars")
+//        let bikes = DataObject(name: "Bikes")
+//        let houses = DataObject(name: "Houses")
+//        let flats = DataObject(name: "Flats")
+//        let motorbikes = DataObject(name: "motorbikes")
+//        let drinks = DataObject(name: "Drinks")
+//        let food = DataObject(name: "Food")
+//        let sweets = DataObject(name: "Sweets")
+//        let watches = DataObject(name: "Watches")
+//        let walls = DataObject(name: "Walls")
+        
+        return [phones, object, cars]
+    }
+    
 }
